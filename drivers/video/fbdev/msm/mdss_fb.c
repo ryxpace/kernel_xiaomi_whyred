912b70b24824419959c2dea4818124ddb17ba61e
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-20 10:25:50 -0700

    msm: mdss: Boost DDR bus when committing a new frame
    In order to reduce jank, request a DDR bus boost whenever a new frame is
    ready to be rendered to the display. The boost should be sufficient
    enough to render 60 FPS without any dropped frames when there is no
    significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -48,6 +48,7 @@
 #include <linux/dma-buf.h>
 #include <linux/mdss_io_util.h>
 #include <linux/wakelock.h>
+#include <linux/devfreq_boost.h>
 #include <sync.h>
 #include <sw_sync.h>
 
@@ -990,7 +991,7 @@ int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, in
 		pr_err("%s,not available\n",__func__);
 		return -1;
 	}
-	
+
 	if((first_ce_state != -1) || (first_cabc_state != -1) || (first_srgb_state != -1) || (first_gamma_state != -1))
 		printk("%s,first_ce_state: %d,first_cabc_state: %d,first_srgb_state=%d,first_gamma_state=%d\n",__func__,
 			first_ce_state,first_cabc_state,first_srgb_state,first_gamma_state);
@@ -1014,7 +1015,7 @@ int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, in
 		default:
 			pr_debug("unknow cmds: %d\n", first_ce_state);
 			break;
-			
+
 	}
 	switch(first_cabc_state) {
 		case 0x1:
@@ -1030,7 +1031,7 @@ int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, in
 		default:
 			pr_debug("unknow cmds: %d\n", first_cabc_state);
 			break;
-			
+
 	}
 	switch(first_srgb_state) {
 		case 0x1:
@@ -1046,7 +1047,7 @@ int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, in
 		default:
 			pr_debug("unknow cmds: %d\n", first_srgb_state);
 			break;
-			
+
 	}
 
 	switch(first_gamma_state) {
@@ -1059,7 +1060,7 @@ int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, in
 		default:
 			pr_debug("unknow cmds: %d\n", first_gamma_state);
 			break;
-			
+
 	}
 	switch(first_cabc_movie_state) {
 		case 0x1:
@@ -1150,7 +1151,7 @@ static ssize_t mdss_fb_set_ce(struct device *dev,struct device_attribute *attr,c
 		pr_err("%s,wait first_set_bl\n",__func__);
 		return len;
 	}
- 
+
 	pr_err("tsx_###_%s,set_ce_cmd: %d\n",__func__, param);
 
 	if(ce_resume){
@@ -1173,7 +1174,7 @@ static ssize_t mdss_fb_set_ce(struct device *dev,struct device_attribute *attr,c
 		default:
 			pr_err("unknow cmds: %d\n", param);
 			break;
-			
+
 	}
 	printk("tsx ##### ce over ###\n");
 	return len;
@@ -1255,7 +1256,7 @@ static ssize_t mdss_fb_set_cabc(struct device *dev,struct device_attribute *attr
 		default:
 			pr_err("unknow cmds: %d\n", param);
 			break;
-			
+
 	}
 	printk("guorui ##### cabc over ###\n");
 	return len;
@@ -1337,7 +1338,7 @@ static ssize_t mdss_fb_set_srgb(struct device *dev,struct device_attribute *attr
 		default:
 			pr_err("unknow cmds: %d\n", param);
 			break;
-			
+
 	}
 	printk("guorui ##### srgb over ###\n");
 	return len;
@@ -2900,7 +2901,7 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	int ret;
 	struct mdss_panel_data *pdata;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
-	
+
 	if ((info == prim_fbi) && (blank_mode == FB_BLANK_UNBLANK) &&
 		atomic_read(&prim_panel_is_on)) {
 		atomic_set(&prim_panel_is_on, false);
@@ -5884,6 +5885,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
