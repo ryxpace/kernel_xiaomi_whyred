da795240bb2962714d1eee01440a93c5cf96e35c
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-07-05 00:26:08 +0600

    icnss: Don't create debugfs when !CONFIG_ICNSS_DEBUG
    The debugfs related codes are guarded using CONFIG_ICNSS_DEBUG
    macro. Below that, there is another chunk of codes for debugfs.
    If we disable CONFIG_DEBUG_FS, it will still execute icnss_debugfs_create()
    because of the else-condition of the ifdeffery. Therefore, remove the
    irrelevant function and make ICNSS_DEBUG depend on DEBUG_FS.
    Guard the debugfs bits accordingly.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Id8a3caaee6e7c99af07fd80db5e24534cf9c6d1f
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -23,7 +23,9 @@
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#ifdef CONFIG_ICNSS_DEBUG
 #include <linux/debugfs.h>
+#endif
 #include <linux/seq_file.h>
 #include <linux/slab.h>
 #include <linux/platform_device.h>
@@ -4569,32 +4571,12 @@ static int icnss_debugfs_create(struct icnss_priv *priv)
 out:
 	return ret;
 }
-#else
-static int icnss_debugfs_create(struct icnss_priv *priv)
-{
-	int ret = 0;
-	struct dentry *root_dentry;
-
-	root_dentry = debugfs_create_dir("icnss", NULL);
-
-	if (IS_ERR(root_dentry)) {
-		ret = PTR_ERR(root_dentry);
-		icnss_pr_err("Unable to create debugfs %d\n", ret);
-		return ret;
-	}
-
-	priv->root_dentry = root_dentry;
-
-	debugfs_create_file("stats", 0600, root_dentry, priv,
-			    &icnss_stats_fops);
-	return 0;
-}
-#endif
 
 static void icnss_debugfs_destroy(struct icnss_priv *priv)
 {
 	debugfs_remove_recursive(priv->root_dentry);
 }
+#endif
 
 static int icnss_get_vbatt_info(struct icnss_priv *priv)
 {
@@ -4825,9 +4807,9 @@ static int icnss_probe(struct platform_device *pdev)
 	}
 
 	icnss_enable_recovery(priv);
-
+#ifdef CONFIG_ICNSS_DEBUG
 	icnss_debugfs_create(priv);
-
+#endif
 	ret = device_init_wakeup(&priv->pdev->dev, true);
 	if (ret)
 		icnss_pr_err("Failed to init platform device wakeup source, err = %d\n",
@@ -4856,9 +4838,9 @@ static int icnss_remove(struct platform_device *pdev)
 	icnss_pr_info("Removing driver: state: 0x%lx\n", penv->state);
 
 	device_init_wakeup(&penv->pdev->dev, false);
-
+#ifdef CONFIG_ICNSS_DEBUG
 	icnss_debugfs_destroy(penv);
-
+#endif
 	complete_all(&penv->unblock_shutdown);
 
 	icnss_modem_ssr_unregister_notifier(penv);
