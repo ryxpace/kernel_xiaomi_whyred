6222182ad734f246099fa67a4d6519eb60664c03
ryxpace <raydawn322@gmail.com>
2023-07-19 20:49:37 +0900

    fs: update implementation
    ref: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    and add macro
    #ifdef CONFIG_KSU

--- fs/exec.c
+++ fs/exec.c
@@ -1527,8 +1527,14 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
+		void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1543,10 +1549,15 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
